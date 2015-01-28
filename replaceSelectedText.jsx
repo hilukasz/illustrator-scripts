47a03387c03546ac037965faa2cfffe5aae23284
Łukasz Wieczorek <lukasz@hellowoo.com>
2015-01-27 16:19:31 -0800

    added error catching

--- replaceSelectedText.jsx
+++ replaceSelectedText.jsx
@@ -1,10 +1,13 @@
-﻿var newContent;
- function getNewContent(){
-        newContent = prompt('new content','');
-  }getNewContent();
+﻿if ( app.activeDocument.selection[i] > 0){
 
-alert(newContent);
+    var newContent;
+     function getNewContent(){
+            newContent = prompt('new content','');
+      }getNewContent();
 
-for(var i = 0; i < app.activeDocument.selection.length; i++){
-    app.activeDocument.selection[i].contents = newContent;
- }
+    
+    for(var i = 0; i < app.activeDocument.selection.length; i++){
+        app.activeDocument.selection[i].contents = newContent;
+    }
+}
+else { alert("yo dawg, I heard through the grapevine you hate selecting text. You should do that. Else this script doesn't work");}
