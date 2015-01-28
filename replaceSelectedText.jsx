52a5b8ae823117d8f0d400307cc54b51415920a8
Łukasz Wieczorek <lukasz@hellowoo.com>
2015-01-27 16:12:19 -0800

    init commit

---
+++ replaceSelectedText.jsx
@@ -0,0 +1,10 @@
+﻿var newContent;
+ function getNewContent(){
+        newContent = prompt('new content','');
+  }getNewContent();
+
+alert(newContent);
+
+for(var i = 0; i < app.activeDocument.selection.length; i++){
+    app.activeDocument.selection[i].contents = newContent;
+ }
